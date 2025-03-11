378fe2e7cafd4045588b7f549a5a3787fe33660a
Rachid Hayoun <rachid.hayoun@laplateforme.io>
2025-03-11 10:01:18 +0100

    Rachid push main

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import pygame
 from config import *
 from ui.menu import bg
+from img import *
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("RPG")
 
-running = True
 background = bg()
+
+running = True
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    background.run_game()  
     pygame.display.update()
